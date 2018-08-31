c859f3c16786a63cba301bb4d77e0ff6a75972ae
BikeMixel <bixelmike@gmail.com>
2018-08-31 17:54:56 -0400

    closer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,48 +1,78 @@
-var timer = 10
-var intervalId;
+var time = 10
+var intervalId
 var correct = 0 
 var incorrect = 0
 
-var triviaArray = 
-    {
-        question: ["what is my name", "what is my favorite color", "wanna fight about it"],
-        a: ["mike", "green", "yes"],
-        b: ["steve", "blue", "no"],
-        c: ["paul", "yellow", "maybe"]
-    }
-
 var timeHold = $("#timeLeft")
-var incorrectHold = $("#incorrectNum")
-incorrectHold.html("You have " + incorrect + " incorrect answers")
-var correctHold = $("#correctNum")
-correctHold.html("You have " + correct + " correct answers")
+var start = $("#startBtn")
+var showHold = $("#testArea")
+var answerHold = $("#answers")
 
-var btnA = $("#btnA")
-var btnB = $("#btnB")
-var btnC = $("#btnC")
 
-var questionZone = $("#questionArea")
-function start() {
-    show();
-    run();
+var triviaArray = {
+    questions: ["1. Who am I?","2. Where am I?","3. How am I?","4. What day is it?","5. What's my favorite color?"],
+    a: ["Mike","Hell","Ok","Tuesday","Blue"],
+    b: ["Steve","The Bathroom","Swell","Monday","Purple"],
+    c: ["Paul","Mars","Awful","Friday","Green"]
 }
-function show () {
-    questionZone.html(triviaArray.question[0])
-    btnA.html(triviaArray.a[0])
-    btnB.html(triviaArray.b[0])
-    btnC.html(triviaArray.c[0])
+var questLen = triviaArray.questions.length
+var ansLen = triviaArray.a.length
+
+var answers = ["Mike","The Bathroom","Swell","Friday","Green"]
+var userAns = []
+
+timeHold.html("You have " + time + " to finish")
+
+start.on('click', game)
+
+function game(){
+    run()
+    show()
 }
-function run() {
-    intervalId = setInterval(time, 1000)
+
+function run(){
+    start.remove()
+    clearInterval(intervalId)
+    intervalId = setInterval(timer, 1000)
 }
-function time() {
-    timer--
-    $("#timeLeft").html("Time Left: " + timer + " seconds")
-    if (timer <= 0) {
-        endTime();
+function timer(){
+    time--
+    timeHold.html("You have " + time + " seconds left")
+    if(time <= 0){
+        end()
     }
 }
-function endTime() {
+function end(){
     clearInterval(intervalId)
 }
-$("#startbtn").on("click", start)
+
+function show(){
+    for(var i = 0; i < questLen; i++){
+        showHold.append("<div>" + triviaArray.questions[i] + "</div")
+                .append("<button id='aBtn'>" + triviaArray.a[i] + "</button>")
+                .append("<button id='bBtn'>" + triviaArray.b[i] + "</button>")
+                .append("<button id='cBtn'>" + triviaArray.c[i] + "</button>")
+    }
+    for (var j = 0; j < ansLen; j++)
+    $("#aBtn").on('click', function(){
+        userAns.push(triviaArray.a[j])
+        console.log(userAns)
+        $("#bBtn").remove()
+        $("#cBtn").remove()
+        $("#aBtn").css("background-color", "green")
+    })
+    $("#bBtn").on('click', function(){
+        userAns.push(triviaArray.b[j])
+        console.log(userAns)
+        $("#aBtn").remove()
+        $("#cBtn").remove()
+        $("#bBtn").css("background-color", "green")
+    })
+    $("#cBtn").on('click', function(){
+        userAns.push(triviaArray.c[j])
+        console.log(userAns)
+        $("#aBtn").remove()
+        $("#bBtn").remove()
+        $("#cBtn").css("background-color", "green")
+    })
+}
